f95a0e40c3b0da7515ec2559faf588bebadff27b
Frenzy <firenzehiga@gmail.com>
2025-01-15 15:20:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // import express and router
 const express = require("express");
 const router = require("./routes/api");
-const protectedRoute = require("./routes/protected");
-const loginRoute = require("./routes/auth/login");
 // import dotenv dan menjalankan method config
 require("dotenv").config();
 
@@ -11,11 +9,6 @@ const app = express();
 
 // menggunakan middleware
 app.use(express.json());
-
-// Rute login
-app.use(loginRoute);  
-
-app.use(protectedRoute);  // Rute yang dilindungi
 
 // menggunakan router
 app.use(router);
